89b2aace1d37e8fdc4875f33df74e61a988d97c5
xiaolu <183138896@qq.com>
2019-12-27 22:19:35 +0800

    pub

--- publish.py
+++ publish.py
@@ -8,10 +8,10 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
 
-
+data = {'statu': False}
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect("127.0.0.1", 1883, 60)
 # 发布 test主题
-client.publish("test", 'hello')
+client.publish("test", data)
