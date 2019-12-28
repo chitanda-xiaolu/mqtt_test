34e55802c5100dd44dac15914fb9856589426ede
xiaolu <183138896@qq.com>
2019-12-28 14:38:02 +0800

    pub

--- publish.py
+++ publish.py
@@ -8,8 +8,8 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
 
-data = {'statu': False}
-payload = str(data)
+# data = {'statu': False}
+payload = 'socket1true'
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
